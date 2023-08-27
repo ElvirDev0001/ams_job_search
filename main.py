6fe91327766e844ba593e02382d225ce401169e0
Elvir Dev <dev.elvir.github@gmail.com>
2023-08-27 14:50:02 +0200

    grab-the-description

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 import asyncio
 import pandas as pd
 from pyppeteer import launch
-
+import numpy as np
 
 async def get_jobs():
     browser = await launch(headless=True)
     page = await browser.newPage()
 
     job_titles = []
+    job_descriptions = []
 
     for i in range(1, 25):  # Loop through 2 pages for example
         url = f"https://jobs.ams.at/public/emps/jobs?page={i}&query=remote%20100&JOB_OFFER_TYPE=SB_WKO&JOB_OFFER_TYPE=IJ&JOB_OFFER_TYPE=BA&PERIOD=ALL&EMPLOYMENT_RELATIONSHIP=AA&sortField=_SCORE"
         await page.goto(url)
-
-        # Wait for a known element to ensure the page has loaded
-        await page.waitForSelector('h2[role="presentation"]', timeout=5000)
+        await asyncio.sleep(5)  # Wait for 5 seconds
 
         titles = await page.evaluate('''() => {
-            return Array.from(document.querySelectorAll('h2[role="presentation"] a')).map(a => {
-                return a.childNodes[0].nodeValue.trim();
-            });
+            return Array.from(document.querySelectorAll('h2[role="presentation"] a')).map(a => a.childNodes[0].nodeValue.trim());
         }''')
 
-        if titles:
-            job_titles.extend(titles)
-        else:
-            print(f"No titles found on page {i}")
+        descriptions = await page.evaluate('''() => {
+            return Array.from(document.querySelectorAll('div[id^="ams-search-joboffer-summary"]')).map(div => div.innerText.trim());
+        }''')
+
+        for title in titles:
+            job_titles.append(title)
+            # If a description exists for this title, use it; otherwise, use NaN
+            job_descriptions.append(descriptions.pop(0) if descriptions else np.nan)
 
     await browser.close()
 
-    if job_titles:
-        # Save to Excel
-        df = pd.DataFrame({'Job Titles': job_titles})
-        df.to_excel('job_titles.xlsx', index=False)
-    else:
-        print("No job titles found.")
+    df = pd.DataFrame({'Job Titles': job_titles, 'Job Descriptions': job_descriptions})
+    df.to_excel('job_titles_descriptions.xlsx', index=False)
 
-
-# Run the asynchronous function
+# Run the function
 asyncio.get_event_loop().run_until_complete(get_jobs())
