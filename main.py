aecde1543c1267b4749ee2ccd3f94b95c458af01
Elvir Dev <dev.elvir.github@gmail.com>
2023-08-27 15:48:31 +0200

    wait-for-h2-speed-update

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ async def get_jobs():
     job_titles = []
     job_descriptions = []
 
-    for i in range(1, 25):  # Loop through 2 pages for example
+    for i in range(1, 10):  # Loop through 24 pages for example
         url = f"https://jobs.ams.at/public/emps/jobs?page={i}&query=remote%20100&JOB_OFFER_TYPE=SB_WKO&JOB_OFFER_TYPE=IJ&JOB_OFFER_TYPE=BA&PERIOD=ALL&EMPLOYMENT_RELATIONSHIP=AA&sortField=_SCORE"
         await page.goto(url)
-        await asyncio.sleep(5)  # Wait for 5 seconds
+
+        # Wait for h2 with role="presentation" to appear
+        await page.waitForSelector('h2[role="presentation"]', timeout=5000)
 
         titles = await page.evaluate('''() => {
             return Array.from(document.querySelectorAll('h2[role="presentation"] a')).map(a => a.childNodes[0].nodeValue.trim());
